71c6351d164c82f26ecb7eb670b114019f33f279
Tsonewa <tsonewa@gmail.com>
2021-11-02 15:37:47 +0200

    basic project setup

---
+++ src/main/java/com/example/todobulgaria/ToDoBulgariaApplication.java
@@ -0,0 +1,13 @@
+package com.example.todobulgaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoBulgariaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoBulgariaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/todobulgaria/ToDoBulgariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todobulgaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoBulgariaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
