a521da1eb394c545e7aff4eb39e9eaf2ef3252a2
Tsonewa <tsonewa@gmail.com>
2021-11-03 11:45:29 +0200

    login.html changes

---
+++ src/main/java/com/example/todobulgaria/web/UserController.java
@@ -0,0 +1,2 @@
+package com.example.todobulgaria.web;public class UserController {
+}
--- src/main/resources/static/js/login-form-action.js
+++ src/main/resources/static/js/login-form-action.js
@@ -1,7 +1,9 @@
 function openForm() {
-    document.getElementById("myForm").style.display = "block";
+    document.getElementById("myModal").style.display = "block";
+    document.getElementById("myForm").style.display = "flex";
 }
 
 function closeForm() {
+    document.getElementById("myModal").style.display = "none";
     document.getElementById("myForm").style.display = "none";
 }
