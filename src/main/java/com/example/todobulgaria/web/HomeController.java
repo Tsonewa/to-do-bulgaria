69485f059263678109ad8f549e397ab4b6f88a07
Tsonewa <tsonewa@gmail.com>
2021-11-12 21:26:05 +0200

    route home for auth and anonymous user

--- src/main/java/com/example/todobulgaria/web/HomeController.java
+++ src/main/java/com/example/todobulgaria/web/HomeController.java
@@ -1,20 +1,28 @@
 package com.example.todobulgaria.web;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
     public String index(){
-    return "index";
-}
+            return "index"; }
 
     @GetMapping("/home")
     public String home(){
+
         return "auth-home";
-}
+    }
 
     @GetMapping("/about-us")
     public String aboutUs(){
@@ -24,6 +32,7 @@ public class HomeController {
 
     @GetMapping("/profile")
     public String getAdminProfile(){
+
         return "profile";
     }
 
