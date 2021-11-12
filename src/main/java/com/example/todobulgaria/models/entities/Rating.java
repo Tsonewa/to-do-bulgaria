de2fa31b27db72a2918a24419cbe6a769cbda21e
Tsonewa <tsonewa@gmail.com>
2021-11-12 21:28:25 +0200

    add rating class

---
+++ src/main/java/com/example/todobulgaria/models/entities/Rating.java
@@ -0,0 +1,17 @@
+package com.example.todobulgaria.models.entities;
+
+public class Rating {
+
+        private float stars;
+
+    public Rating() {
+    }
+
+    public float getStars() {
+        return stars;
+    }
+
+    public void setStars(float stars) {
+        this.stars = stars;
+    }
+}
