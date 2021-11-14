2799da2e4b17e2d75998114f330eb8f837e94cc1
Tsonewa <tsonewa@gmail.com>
2021-11-14 21:21:02 +0200

    search template/SearchParamDto

---
+++ src/main/java/com/example/todobulgaria/models/dto/SearchParamsDto.java
@@ -0,0 +1,46 @@
+package com.example.todobulgaria.models.dto;
+
+import java.time.LocalDate;
+
+public class SearchParamsDto {
+
+    private String townName;
+    private LocalDate start;
+    private LocalDate end;
+    private String categoryName;
+
+    public SearchParamsDto() {
+    }
+
+    public String getTownName() {
+        return townName;
+    }
+
+    public void setTownName(String townName) {
+        this.townName = townName;
+    }
+
+    public LocalDate getStart() {
+        return start;
+    }
+
+    public void setStart(LocalDate start) {
+        this.start = start;
+    }
+
+    public LocalDate getEnd() {
+        return end;
+    }
+
+    public void setEnd(LocalDate end) {
+        this.end = end;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+}
