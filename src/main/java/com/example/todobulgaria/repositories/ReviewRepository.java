6458ae2e4295a810bd0ed0aa6a38e3df444c8cf7
Tsonewa <tsonewa@gmail.com>
2021-11-21 12:03:07 +0200

    create reviews repository / get all review by trip id

---
+++ src/main/java/com/example/todobulgaria/repositories/ReviewRepository.java
@@ -0,0 +1,13 @@
+package com.example.todobulgaria.repositories;
+
+import com.example.todobulgaria.models.entities.ReviewEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<ReviewEntity, Long> {
+
+    List<ReviewEntity> getAllByTripId(Long tripId);
+}
