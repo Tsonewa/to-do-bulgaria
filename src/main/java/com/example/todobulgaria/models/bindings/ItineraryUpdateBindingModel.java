9c08fc32b6dfea2d06b7cda54453f0d907330579
Tsonewa <tsonewa@gmail.com>
2021-11-24 17:28:14 +0200

    add validation for update itinerary

--- src/main/java/com/example/todobulgaria/models/bindings/ItineraryUpdateBindingModel.java
+++ src/main/java/com/example/todobulgaria/models/bindings/ItineraryUpdateBindingModel.java
@@ -1,22 +1,36 @@
 package com.example.todobulgaria.models.bindings;
 
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 public class ItineraryUpdateBindingModel {
 
     private Long id;
+    @Size(min = 2, max = 50, message = "Name length must be between 2 and 50 symbols.")
     private String breakfastPlace;
+    @Size(max = 100, message = "Address length must be less than 100 symbols.")
     private String breakfastPlaceAddress;
+    @Size(max=200, message = "Url length must be less than 200 symbols")
     private String breakfastPlaceBookingUrl;
+    @Size(min = 2, max = 50, message = "Name length must be between 2 and 50 symbols.")
     private String coffeePlace;
+    @Size(max = 100, message = "Address length must be less than 100 symbols.")
     private String coffeePlaceAddress;
+    @Size(max=200, message = "Url length must be less than 200 symbols")
     private String coffeePlaceBookingUrl;
+    @Size(min = 2, max = 50, message = "Name length must be between 2 and 50 symbols.")
     private String dinnerPlace;
+    @Size(max = 100, message = "Address length must be less than 100 symbols.")
     private String dinnerPlaceAddress;
+    @Size(max=200, message = "Url length must be less than 200 symbols")
     private String dinnerPlaceBookingUrl;
+    @Size(max = 100, message = "Address length must be less than 100 symbols.")
     private String hotel;
+    @Size(max = 100, message = "Address length must be less than 100 symbols.")
     private String hotelAddress;
+    @Size(max=200, message = "Url length must be less than 200 symbols")
     private String hotelBookingUrl;
     private Long tripId;
-
 
     public ItineraryUpdateBindingModel() {
     }
--- src/main/resources/static/js/multistep-form.js
+++ src/main/resources/static/js/multistep-form.js
@@ -39,37 +39,37 @@ function addDay(){
     let div = document.createElement('div');
     div.className = "input-div";
 
-    h2.textContent = 'Day ' + n;
+    h2.textContent = 'Ден ' + n;
     h2.className= "fs-title";
     let label = document.createElement('label');
-    label.textContent = "Town";
+    label.textContent = "Град";
     div.appendChild(h2);
     div.appendChild(label);
 
     let townInput = document.createElement('input');
     townInput.type = "text";
     townInput.name = "townName";
-    townInput.placeholder = "Sofia";
+    townInput.placeholder = "София";
     div.appendChild(townInput);
     let labelBrekfast = document.createElement('label');
-    labelBrekfast.textContent = "Breakfast";
+    labelBrekfast.textContent = "Закуска";
 
     let breakfastInput = document.createElement('input');
     breakfastInput.type = "text";
     breakfastInput.name = "breakfastPlace";
-    breakfastInput.placeholder = "Mekitsa&Coffee";
+    breakfastInput.placeholder = "Мекица и Кафе";
     div.appendChild(labelBrekfast).appendChild(breakfastInput);
 
     let labelCoffee = document.createElement('label');
-    labelCoffee.textContent = "Coffee";
+    labelCoffee.textContent = "Кафе";
     let coffeeInput = document.createElement('input');
     coffeeInput.type = "text";
     coffeeInput.name = "coffeePlace";
-    coffeeInput.placeholder = "CoffeeBar";
+    coffeeInput.placeholder = "Кафе бар";
     div.appendChild(labelCoffee).appendChild(coffeeInput);
 
     let hotelCoffee = document.createElement('label');
-    hotelCoffee.textContent = "Hotel";
+    hotelCoffee.textContent = "Хотел";
     let hotelInput = document.createElement('input');
     hotelInput.type = "text";
     hotelInput.name = "hotel";
@@ -77,19 +77,19 @@ function addDay(){
     div.appendChild(hotelCoffee).appendChild(hotelInput);
 
     let dinnerLabel = document.createElement('label');
-    dinnerLabel.textContent = "Dinner";
+    dinnerLabel.textContent = "Вечеря";
     let dinnerInput = document.createElement('input');
     dinnerInput.type = "text";
     dinnerInput.name = "dinnerPlace";
-    dinnerInput.placeholder = "Blue House";
+    dinnerInput.placeholder = "Синята къща";
     div.appendChild(dinnerLabel).appendChild(dinnerInput);
 
     let attractionsLabel = document.createElement('label');
-    attractionsLabel.textContent = "Attractions";
+    attractionsLabel.textContent = "Атракции";
     let attractionsInput = document.createElement('input');
     attractionsInput.type = "text";
     attractionsInput.name = "attractionsName";
-    attractionsInput.placeholder = "National Palace of Culture";
+    attractionsInput.placeholder = "Национален дворец на културата";
     div.appendChild(attractionsLabel).appendChild(attractionsInput);
 
     // // let btn = document.createElement('button');
