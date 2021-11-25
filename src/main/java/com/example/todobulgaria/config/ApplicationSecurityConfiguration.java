023fc0f5a3f683f0f4e7a0cc4b73d2a80b8cf4e8
Tsonewa <tsonewa@gmail.com>
2021-11-25 17:14:41 +0200

    config spring boot actuator

--- src/main/java/com/example/todobulgaria/config/ApplicationSecurityConfiguration.java
+++ src/main/java/com/example/todobulgaria/config/ApplicationSecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.example.todobulgaria.config;
 
 import com.example.todobulgaria.security.UserDetailsImpl;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -11,6 +12,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.header.writers.StaticHeadersWriter;
+
+import javax.websocket.Endpoint;
 
 @Configuration
 @EnableWebSecurity
@@ -37,7 +41,7 @@ public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapt
 http.
         authorizeRequests().
                         requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll().
-                        antMatchers("/", "/users/login", "/users/register", "/about-us", "/trips/best").permitAll().
+                        antMatchers("/", "/users/login", "/users/register", "/about-us", "/trips/best", "/actuator/**", "/admin").permitAll().
                         antMatchers("/**").authenticated().
                 and().
                         formLogin().
@@ -52,5 +56,13 @@ http.
                         logoutSuccessUrl("/").
                         invalidateHttpSession(true).
                         deleteCookies("JSESSIONID");
+
+
+//        .headers()
+//                .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Origin", "*"))
+//                .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Methods", "POST, GET", "PUT", "PATCH"))
+//                .addHeaderWriter(new StaticHeadersWriter("Access-Control-Max-Age", "3600"))
+//                .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Credentials", "false"))
+//                .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Headers", "Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization"));
     }
 }
