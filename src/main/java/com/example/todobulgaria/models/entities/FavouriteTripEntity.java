830a057c1dc851bc0b3dd0c79842edaceb4c68d5
Tsonewa <tsonewa@gmail.com>
2021-11-26 22:02:31 +0200

    minor login template style changes

--- src/main/java/com/example/todobulgaria/models/entities/FavouriteTripEntity.java
+++ src/main/java/com/example/todobulgaria/models/entities/FavouriteTripEntity.java
@@ -2,10 +2,7 @@ package com.example.todobulgaria.models.entities;
 
 import com.example.todobulgaria.models.BaseEntity;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.ManyToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -14,17 +11,4 @@ import java.util.Set;
 @Table(name = "favourites")
 public class FavouriteTripEntity extends BaseEntity {
 
-    @ManyToMany
-    private Set<UserEntity> userFavourites = new HashSet<>();
-
-    public FavouriteTripEntity() {
-    }
-
-    public Set<UserEntity> getUserFavourites() {
-        return userFavourites;
-    }
-
-    public void setUserFavourites(Set<UserEntity> userFavourites) {
-        this.userFavourites = userFavourites;
-    }
 }
