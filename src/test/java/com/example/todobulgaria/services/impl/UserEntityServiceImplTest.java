6292998452bf20e65ed545b66e24813f8f1184b9
Tsonewa <tsonewa@gmail.com>
2021-11-27 21:17:02 +0200

    set up junit

---
+++ src/test/java/com/example/todobulgaria/services/impl/UserEntityServiceImplTest.java
@@ -0,0 +1,24 @@
+package com.example.todobulgaria.services.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserEntityServiceImplTest {
+
+    @Test
+    void registrarUser() {
+    }
+
+    @Test
+    void existByUsername() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+
+    @Test
+    void findUserByUsername() {
+    }
+}
